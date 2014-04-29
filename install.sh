c80dec042e248b27d116e2b69d999e3dfe3a46ae
Mickael <mickael@beingenious.com>
2014-04-29 17:41:30 +0200

    added info about LC_TYPE
    Signed-off-by: Mickael <mickael@beingenious.com>

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ npm install
 cd node
 npm install
 cd ..
+export LC_CTYPE=en_US.UTF-8
+echo "Be sure to have your LC_TYPE env variable set. (export LC_CTYPE=en_US.UTF-8)\n"
 echo "Now you will need to launch the followings : \n"
 echo "Redis (redis-server)\n"
 echo "the NodeJS server (cd node; npm start)\n"
