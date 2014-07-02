e523df5bb6de1101427b9bc586ed07e381db83cc
Micka33 <mickael.cassy@gmail.com>
2014-07-02 12:35:40 +0200

    video_thumbnail_url

--- rails/elasticsearh_api/app/worker/create_video_thumbnail.rb
+++ rails/elasticsearh_api/app/worker/create_video_thumbnail.rb
@@ -7,7 +7,7 @@ class CreateVideoThumbnail
     path = './tmp/videos_thumbnails'
     output_document = "#{course_id}.jpeg"
 
-    if course.video_url.nil? || course.video_url.empty?
+    if course.video_thumbnail_url.nil? || course.video_thumbnail_url.empty?
       video_duration = `avprobe -loglevel error -show_streams 537f66f76b73334071100000_mp4_h264_aac_hd.mp4 | grep duration | cut -f 2 -d = | cut -f 1 -d . | head -1`.strip
       `avconv -y -ss #{(video_duration / 2).round} -i #{course.video_url_ori} -vframes 1 -f image2 #{path+'/'+output_document} 2>&1`
       Course.find(course_id).update_attributes(video_thumbnail_url: output_document)
